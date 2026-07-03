04975a66263fda02fcb29751aee45dd5b6a3fb62
Aleksandr Rol <rolaleksandr75@gmail.com>
2026-07-03 20:02:51 +0900

    refactor: update auth flow to use specific API path for Pionex authentication

--- source/infrastructure/http/pionex/auth_strategy.py
+++ source/infrastructure/http/pionex/auth_strategy.py
@@ -5,7 +5,6 @@ from collections.abc import AsyncGenerator
 from httpx import Auth, Request
 from httpx._models import Response
 
-from source.constants import CHECK_GRID_URL, CREATE_GRID_URL
 from source.settings import PionexSettings
 
 
@@ -14,7 +13,7 @@ class CustomPionexAuth(Auth):
         self._settings = settings
 
     async def async_auth_flow(self, request: Request) -> AsyncGenerator[Request, Response]:
-        if request.url.path in {CHECK_GRID_URL, CREATE_GRID_URL}:
+        if request.url.path.startswith("/api/v1/bot/orders"):
             request.headers["PIONEX-KEY"] = self._settings.api_key.get_secret_value()
             request.headers["PIONEX-SIGNATURE"] = self._generate_signature(request)
 
